fix data calculation bugs
- use previous trial's reward rather than current trial's reward as one condition

script/markov_device.py
@@ -27,7 +27,7 @@ from datetime import datetime
 from functools import reduce
 import scipy.optimize as opt
 import itertools
-
+from pandas import CategoricalDtype
 
 STATE = (0, 1, 2)
 TEXT = ("", "A1", "A2", "B1", "B2", "C1", "C2")
@@ -848,11 +848,21 @@ class MarkovACTR(MarkovState):
         Calculate the probability of stay:
             A trial is marked as "STAY" if the agent selects the same action in current trial (e.g. LEFT)
             as the previous trial
+
+        NOTE: will be -1 trials because exclude NA rows
         """
+
         df = self.df_behaviors()
-        df['state1_stay'] = df['state1_response'].shift(-1)
+        df['state1_stay'] = df['state1_response'].shift() # first row is NA (look at previsou trial)
         df['state1_stay'] = df.apply(
             lambda x: 1 if x['state1_stay'] == x['state1_response'] else (np.nan if pd.isnull(x['state1_stay']) else 0), axis=1)
+        df['pre_received_reward'] = df['received_reward'].shift()
+
+        df = df.dropna(subset=['state1_stay', 'pre_received_reward'])
+        df['pre_received_reward'] = df.apply(lambda x: 'non-reward' if x['pre_received_reward'] == 0 else 'reward', axis=1)
+        df = df.astype({'state_frequency': CategoricalDtype(categories=['common', 'rare'], ordered=True),
+                        'pre_received_reward': CategoricalDtype(categories=['reward', 'non-reward'], ordered=True)})
+        df = df[['index', 'state_frequency', 'received_reward', 'pre_received_reward', 'state1_stay']]
         return df
 
     def calculate_real_frequency(self, merge=False):

script/markov_simulate_test.py
@@ -2,6 +2,7 @@
 
 import matplotlib.pyplot as plt
 import seaborn as sns
+from pandas import CategoricalDtype
 from plotnine import *
 from plotnine.data import *
 from pathlib import Path
@@ -58,39 +59,39 @@ def try_simulation_example():
     return m1, m2, m3
 
 
-def simulate_response_monkey(model="markov-monkey", epoch=1, n=20):
-    rewards = 0.0
-    beh_list, state1stay_list = [], []
-
-    start_time = time.time()
-    for i in tqdm(range(epoch)):
-        m = MarkovHuman(model, verbose=False)
-        m.run_experiment(n)
-        if (i == 0): print(m)
-        m.run_experiment(n)
-
-        state1stay = m.calculate_stay_probability()
-        beh = m.df_postprocess_behaviors()
-        state1stay['epoch'] = i
-        beh['epoch'] = i
-
-        beh_list.append(beh)
-        state1stay_list.append(
-            # state1stay.groupby(['epoch', 'received_reward', 'reward_frequency', 'state_frequency']).agg(
-            state1stay.groupby(['epoch', 'received_reward', 'state_frequency']).agg(
-                {'state1_stay': lambda x: x.mean(skipna=True)}).reset_index())
-
-        rewards += beh['received_reward'].sum() / len(beh)
-
-    max_id = np.argmax(list(m.task_parameters['REWARD_PROBABILITY'].values()))
-    expected_reward = list(m.task_parameters['REWARD'].values())[max_id] * \
-                      list(m.task_parameters['REWARD_PROBABILITY'].values())[max_id]
-    print('>>>>>>>>> SIMULATION REWARD GAINED <<<<<<<<<< \t EPOCH:', epoch)
-    print('GAINED R: %.2f (EXPECTED R: %.2f) \t [%.2f %%]\n\n\n\n' % (
-    (rewards / epoch), expected_reward, 100 * (rewards / epoch) / expected_reward))
-    df_beh = pd.concat(beh_list, axis=0)
-    df_state1stay = pd.concat(state1stay_list, axis=0)
-    return df_beh, df_state1stay
+# def simulate_response_monkey(model="markov-monkey", epoch=1, n=20):
+#     rewards = 0.0
+#     beh_list, state1stay_list = [], []
+#
+#     start_time = time.time()
+#     for i in tqdm(range(epoch)):
+#         m = MarkovHuman(model, verbose=False)
+#         m.run_experiment(n)
+#         if (i == 0): print(m)
+#         m.run_experiment(n)
+#
+#         state1stay = m.calculate_stay_probability()
+#         beh = m.df_postprocess_behaviors()
+#         state1stay['epoch'] = i
+#         beh['epoch'] = i
+#
+#         beh_list.append(beh)
+#         state1stay_list.append(
+#             # state1stay.groupby(['epoch', 'received_reward', 'reward_frequency', 'state_frequency']).agg(
+#             state1stay.groupby(['epoch', 'received_reward', 'state_frequency']).agg(
+#                 {'state1_stay': lambda x: x.mean(skipna=True)}).reset_index())
+#
+#         rewards += beh['received_reward'].sum() / len(beh)
+#
+#     max_id = np.argmax(list(m.task_parameters['REWARD_PROBABILITY'].values()))
+#     expected_reward = list(m.task_parameters['REWARD'].values())[max_id] * \
+#                       list(m.task_parameters['REWARD_PROBABILITY'].values())[max_id]
+#     print('>>>>>>>>> SIMULATION REWARD GAINED <<<<<<<<<< \t EPOCH:', epoch)
+#     print('GAINED R: %.2f (EXPECTED R: %.2f) \t [%.2f %%]\n\n\n\n' % (
+#     (rewards / epoch), expected_reward, 100 * (rewards / epoch) / expected_reward))
+#     df_beh = pd.concat(beh_list, axis=0)
+#     df_state1stay = pd.concat(state1stay_list, axis=0)
+#     return df_beh, df_state1stay
 
 
 def simulate_stay_probability(model="markov-model1", epoch=1, n=20, task_params=None, actr_params=None, log=False):
@@ -110,7 +111,12 @@ def simulate_stay_probability(model="markov-model1", epoch=1, n=20, task_params=
         utrace['epoch'] = i
         atrace['epoch'] = i
         beh['epoch'] = i
-        
+
+        # subset
+        # state1stay = state1stay.groupby(['epoch', 'pre_received_reward', 'state_frequency']).agg(
+        #     {'state1_stay': lambda x: x.mean(skipna=True)}).reset_index()
+
+
         if log:
             df_beh, df_state1stay, df_utrace, df_atrace = beh, state1stay, utrace, atrace
             file_path = log_simulation(beh, dir_path=log, file_name=model+'-sim-logdata')
@@ -120,19 +126,18 @@ def simulate_stay_probability(model="markov-model1", epoch=1, n=20, task_params=
             
         else:
             beh_list.append(beh)
-            # state1stay_list.append(state1stay.groupby(['epoch', 'm_parameter', 'received_reward', 'reward_frequency', 'state_frequency']).agg({'state1_stay': lambda x: x.mean(skipna=True)}).reset_index())
-            state1stay_list.append(state1stay.groupby(['epoch', 'm_parameter', 'received_reward', 'state_frequency']).agg({'state1_stay': lambda x: x.mean(skipna=True)}).reset_index())
-            utrace_list.append(utrace.groupby(['epoch', 'index', 'action', 'state', 'response']).agg({':utility': lambda x: x.max(skipna=True)}).reset_index())
-            atrace_list.append(atrace.groupby(['epoch', 'index', 'memory', 'memory_type']).agg({':Reference-Count': lambda x: x.max(skipna=True),
-                                                                                                ':Activation': lambda x: x.max( skipna=True),
-                                                                                                ':Last-Retrieval-Activation': lambda x: x.max(skipna=True)}).reset_index())
+            state1stay_list.append(state1stay)
+            utrace_list.append(utrace)
+            atrace_list.append(atrace)
 
-            # state1stay_list.append(state1stay)
-            # utrace_list.append(utrace)
-            # atrace_list.append(atrace)
+            # state1stay_list.append(state1stay.groupby(['epoch', 'm_parameter', 'received_reward', 'reward_frequency', 'state_frequency']).agg({'state1_stay': lambda x: x.mean(skipna=True)}).reset_index())
+            #state1stay_list.append(state1stay.groupby(['epoch', 'pre_received_reward', 'state_frequency']).agg({'state1_stay': lambda x: x.mean(skipna=True)}).reset_index())
+            #utrace_list.append(utrace.groupby(['epoch', 'action', 'state', 'response']).agg({':utility': lambda x: x.max(skipna=True)}).reset_index())
+            #atrace_list.append(atrace.groupby(['epoch', 'memory', 'memory_type']).agg({':Reference-Count': lambda x: x.max(skipna=True), ':Activation': lambda x: x.max( skipna=True), ':Last-Retrieval-Activation': lambda x: x.max(skipna=True)}).reset_index())
 
             # plot
             rewards += beh['received_reward'].sum()/len(beh)
+
         
     if log:
         log_params(dir_path=log, epoch=epoch, n=n, actr_params=actr_params, task_params=task_params, file_path=file_path)
@@ -196,10 +201,13 @@ def log_params(dir_path, epoch, n, actr_params, task_params, file_path, verbose=
 
 def load_simulation(data_path='data/param_simulation_1114/param_id0', model_name='markov-model1', index_thres=None, verbose=True):
     assert (os.getcwd().split("/")[-1] == 'ACTR-MarkovTask')
-    df1 = pd.read_csv(os.path.join(data_path, model_name + '-sim-logdata.csv')).dropna(axis=0).apply(pd.to_numeric,
-                                                                                                     errors='ignore')
-    df1_state1stay = pd.read_csv(os.path.join(data_path, model_name + '-sim-staydata.csv')).dropna(axis=0).apply(
-        pd.to_numeric, errors='ignore')
+    df1 = pd.read_csv(os.path.join(data_path, model_name + '-sim-logdata.csv')).dropna(axis=0).apply(pd.to_numeric, errors='ignore')
+    df1_state1stay = pd.read_csv(os.path.join(data_path, model_name + '-sim-staydata.csv')).dropna(axis=0).apply(pd.to_numeric, errors='ignore')
+
+    # temporarily fix 0115 simulation stay probability errors
+    if 'pre_received_reward' not in df1.columns:
+        print('temporarily fix 0115 simulation stay probability errors')
+        df1_state1stay = temporary_update_stay_probability(df1)
 
     if index_thres:
         df1 = df1[df1['index'] < index_thres]
@@ -219,3 +227,47 @@ def load_simulation(data_path='data/param_simulation_1114/param_id0', model_name
         print('...SUCCESSFULLY LOADED DATA...')
         print(param_log)
     return df1, df1_state1stay, df1_utrace, df1_atrace
+
+def check_parameters(log_file_path, task_param_set, actr_param_set, epoch, n):
+    if not os.path.exists(log_file_path):
+        return False
+    log = pd.read_csv(log_file_path, header=0, index_col=0).drop(columns=['file_path'])
+    log_param_list = log.to_records(index=False).tolist()
+    for log_param_set in log_param_list:
+        curr_param_set = (epoch, n,
+                          actr_param_set['seed'],
+                          actr_param_set['ans'],
+                          actr_param_set['egs'],
+                          actr_param_set['alpha'],
+                          # actr_param_set['v'],
+                          actr_param_set['lf'],
+                          actr_param_set['bll'],
+                          actr_param_set['mas'],
+                          str(task_param_set['REWARD']),
+                          task_param_set['RANDOM_WALK'],
+                          task_param_set['M'])
+
+        if log_param_set == curr_param_set:
+            return True
+    return False
+
+
+def temporary_update_stay_probability(df):
+    """
+    Only apply to 0115 simulation
+    Calculate the probability of stay:
+        A trial is marked as "STAY" if the agent selects the same action in current trial (e.g. LEFT)
+        as the previous trial
+    """
+    df['state1_stay'] = df['state1_response'].shift() # first row is NA (look at previsou trial)
+    df['state1_stay'] = df.apply(
+        lambda x: 1 if x['state1_stay'] == x['state1_response'] else (np.nan if pd.isnull(x['state1_stay']) else 0),
+        axis=1)
+    df['pre_received_reward'] = df['received_reward'].shift()
+
+    df = df.dropna(subset=['state1_stay', 'pre_received_reward'])
+    df.loc[:, ['pre_received_reward']] = df.apply(lambda x: 'non-reward' if int(x['pre_received_reward']) == 0 else 'reward', axis=1)
+    df = df.astype({'state_frequency': CategoricalDtype(categories=['common', 'rare'], ordered=True),
+                    'pre_received_reward': CategoricalDtype(categories=['reward', 'non-reward'], ordered=True)})
+    df = df[['epoch', 'index', 'state_frequency', 'received_reward', 'pre_received_reward', 'state1_stay']]
+    return df